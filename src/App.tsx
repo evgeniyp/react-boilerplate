d8d344388cc1dfcfea602d0376a009b99c7ce24c
Evgeniy Petukhov <evgeniy.petukhov@gmail.com>
2020-12-26 17:50:34 +0300

    Added eslint, bootstrap, emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {FC} from 'react';
 
-function App() {
+import {css, cx} from '@emotion/css';
+
+const wheat = css({
+  backgroundColor: 'wheat',
+  border: '1px solid'
+});
+
+export const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <div className={cx('col-sm', wheat)}>One of three columns</div>
+        <div className={cx('col-sm', wheat)}>One of three columns</div>
+        <div className={cx('col-sm', wheat)}>One of three columns</div>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
